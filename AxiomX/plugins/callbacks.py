b1ce9f2ec150a83424ba08b7c27d72298a83c8e9
antidote2769-spec <antidote2769@gmail.com>
2026-06-05 09:49:21 +0530

    Update callbacks.py

--- AxiomX/plugins/callbacks.py
+++ AxiomX/plugins/callbacks.py
@@ -316,11 +316,11 @@ async def back_to_start_callback(client, query):
     
     buttons = [
         [ptypes.InlineKeyboardButton(font('＋ 𝐈‌ηᴛєɢꝛᴧᴛє 𝐈‌η 𝐘‌συꝛ 𝐂‌ʜᴧᴛ  ＋'), url=f'https://t.me/{bot_username}?startgroup=true', style=ButtonStyle.SUCCESS)],
-        [ptypes.InlineKeyboardButton(font('🎧 𝐌‌υsɪᴄ'), url=f'https://t.me/vcXmnvbot?start=help', style=ButtonStyle.PRIMARY),
+        [ptypes.InlineKeyboardButton(font('🎧 𝐌‌υsɪᴄ'), url=f'https://t.me/AR_MUSIC_27BOT?start=help', style=ButtonStyle.PRIMARY),
          ptypes.InlineKeyboardButton(font('⚙ 𝐇‌єʟᴘ ⚙'), callback_data=f'help_{user_id}', style=ButtonStyle.DANGER)],
          [ptypes.InlineKeyboardButton(font('𝐔‌ᴘᴅᴧᴛєs ⎘'), url=update_url, style=ButtonStyle.PRIMARY),
          ptypes.InlineKeyboardButton(font('𝐒‌υᴘᴘσꝛᴛ ☏︎'), url=support_url, style=ButtonStyle.PRIMARY)],
-        [ptypes.InlineKeyboardButton(font('σᴡηєꝛ ✧ ᴀxɪσϻ'), user_id=config.AXIOM_UID, style=ButtonStyle.SUCCESS)]
+        [ptypes.InlineKeyboardButton(font('σᴡηєꝛ ✧ ᴀʀ), user_id=config.AXIOM_UID, style=ButtonStyle.SUCCESS)]
     ]
     caption = (
         f"<blockquote><b>⍣ 𝐇‌єʏᴧ {mention}\n"
